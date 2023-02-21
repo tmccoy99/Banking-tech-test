ff7134d54b19c4d9ee2d1f223c5543308fa61ec5
Thomas McCoy <finnmccoy99@gmail.com>
2023-02-21 14:11:30 +0000

    Tested and implemented proper display of multiple deposits

--- StatementPrinter.js
+++ StatementPrinter.js
@@ -8,10 +8,38 @@ class StatementPrinter {
       );
     }
     this.io = io;
+    this.account = account;
   }
 
   print() {
+    this.#printHeader();
+    const deposits = this.account.getTransactionHistory();
+    for (const deposit of deposits.reverse()) {
+      this.#printFormattedDeposit(deposit);
+    }
+  }
+
+  #printHeader() {
     this.io.log('date || credit || debit || balance');
+  }
+
+  #printFormattedDeposit(deposit) {
+    this.io.log(
+      `${this.#generateDateString(deposit.date)} || ${deposit.amount.toFixed(
+        2
+      )} || || ${deposit.balance.toFixed(2)}`
+    );
+  }
+
+  #generateDateString(date) {
+    let dayString = date.getDate().toString();
+    let monthString = (date.getMonth() + 1).toString();
+    const addLeadingZeroIfNecessary = (string) => {
+      return string.length === 1 ? '0' + string : string;
+    };
+    return `${addLeadingZeroIfNecessary(dayString)}/${addLeadingZeroIfNecessary(
+      monthString
+    )}/${date.getFullYear()}`;
   }
 }
 
--- spec/unit/StatementPrinter.test.js
+++ spec/unit/StatementPrinter.test.js
@@ -33,14 +33,65 @@ describe('StatementPrinter class testing -', () => {
     beforeEach(() => {
       statementPrinter = new StatementPrinter(new Account(), ioMock);
       mockAccountInstance = Account.mock.instances[0];
+      ioMock.log.mockClear();
     });
-    test('calling print when saved Account has no transactions logs only statement header string', () => {
-      mockAccountInstance.getTransactionHistory.mockReturnValueOnce({});
+    test('when saved Account has no transactions logs only statement header string', () => {
+      mockAccountInstance.getTransactionHistory.mockReturnValueOnce([]);
       statementPrinter.print();
       expect(ioMock.log).toHaveBeenCalledWith(
         'date || credit || debit || balance'
       );
       expect(ioMock.log.mock.calls).toHaveLength(1);
+    });
+
+    test('when saved Account has a recorded deposit, the formatted deposit string is logged after the header', () => {
+      mockAccountInstance.getTransactionHistory.mockReturnValueOnce([
+        {
+          type: 'deposit',
+          amount: 50,
+          balance: 50,
+          date: new Date('04 Dec 1995 00:12:00 GMT'),
+        },
+      ]);
+      statementPrinter.print();
+      expect(ioMock.log.mock.calls).toHaveLength(2);
+      expect(ioMock.log.mock.calls[0][0]).toBe(
+        'date || credit || debit || balance'
+      );
+      expect(ioMock.log.mock.calls[1][0]).toBe(
+        '04/12/1995 || 50.00 || || 50.00'
+      );
+    });
+
+    test('when saved Account has multiple recorded deposits, formatted deposit strings are logged in reverse chronological order', () => {
+      mockAccountInstance.getTransactionHistory.mockReturnValueOnce([
+        {
+          type: 'deposit',
+          amount: 50,
+          balance: 50,
+          date: new Date('04 Dec 1995 00:12:00 GMT'),
+        },
+        {
+          type: 'deposit',
+          amount: 55.5,
+          balance: 105.5,
+          date: new Date('06 Dec 1995 00:11:00 GMT'),
+        },
+        {
+          type: 'deposit',
+          amount: 0.25,
+          balance: 105.75,
+          date: new Date('23 Jan 1996 00:12:33 GMT'),
+        },
+      ]);
+      statementPrinter.print();
+      expect(ioMock.log.mock.calls).toHaveLength(4);
+      expect(ioMock.log.mock.calls.flat()).toEqual([
+        'date || credit || debit || balance',
+        '23/01/1996 || 0.25 || || 105.75',
+        '06/12/1995 || 55.50 || || 105.50',
+        '04/12/1995 || 50.00 || || 50.00',
+      ]);
     });
   });
 });
